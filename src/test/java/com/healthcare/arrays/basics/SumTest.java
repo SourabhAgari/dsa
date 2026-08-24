877f3e2b2ad9c2b0d0886d4bd6da5344580805a1
sourabhragari <agarisourabh@gmail.com>
2026-08-24 19:12:08 +0530

    tests for sum of all elements

--- src/test/java/com/healthcare/arrays/basics/SumTest.java
+++ src/test/java/com/healthcare/arrays/basics/SumTest.java
@@ -1,4 +1,17 @@
 package com.healthcare.arrays.basics;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class SumTest {
+
+    @Test
+    public void returnsSumOfAllArrayElements() {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        assertEquals(Sum.getSum(arr),45);
+    }
+
+    public void returnsZeroForEmptyArray() {
+        assertEquals(Sum.getSum(new int[0]),0);
+    }
 }
