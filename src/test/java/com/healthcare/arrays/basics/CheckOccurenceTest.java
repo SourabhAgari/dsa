7d96f75fd03588f93ed630e6744ba49457fbb11d
sourabhragari <agarisourabh@gmail.com>
2026-08-24 19:23:33 +0530

    uformatting changed

--- src/test/java/com/healthcare/arrays/basics/CheckOccurenceTest.java
+++ src/test/java/com/healthcare/arrays/basics/CheckOccurenceTest.java
@@ -9,18 +9,14 @@ public class CheckOccurenceTest {
     @Test
     void countsEveryOccurrenceOfAnElement() {
         int[] numbers = {4, 7, 4, 1, 4, 9};
-
         int count = CheckOccurence.checkOccurence(numbers, 4);
-
         assertEquals(3, count);
     }
 
     @Test
     void returnsZeroWhenElementIsMissing() {
         int[] numbers = {4, 7, 1, 9};
-
         int count = CheckOccurence.checkOccurence(numbers, 2);
-
         assertEquals(0, count);
     }
 }
