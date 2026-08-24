cecbaf18063b48384bb4bf66fedf446ffdb19bd7
sourabhragari <agarisourabh@gmail.com>
2026-08-24 19:17:46 +0530

    tests for even count

--- src/test/java/com/healthcare/arrays/basics/EvenCountTest.java
+++ src/test/java/com/healthcare/arrays/basics/EvenCountTest.java
@@ -1,4 +1,20 @@
 package com.healthcare.arrays.basics;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class EvenCountTest {
+
+    @Test
+    public void evenCountForThreeEvenElements() {
+        int[] arr = new int[] {1,2,3,4,5,6};
+        assertEquals(EvenCount.getEventCount(arr),3);
+    }
+
+    @Test
+    public void zeroCountForZeroElements() {
+        int[] arr = new int[] {};
+        assertEquals(EvenCount.getEventCount(arr),0);
+    }
 }
