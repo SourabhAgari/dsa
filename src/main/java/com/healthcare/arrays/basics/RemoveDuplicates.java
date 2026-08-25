f0c39d0b2c740c5efa6977895f4fc2f46547623a
sourabhragari <agarisourabh@gmail.com>
2026-08-25 16:37:08 +0530

    remove duplicates from an array

--- src/main/java/com/healthcare/arrays/basics/RemoveDuplicates.java
+++ src/main/java/com/healthcare/arrays/basics/RemoveDuplicates.java
@@ -1,4 +1,18 @@
 package com.healthcare.arrays.basics;
 
+import com.healthcare.arrays.utils.Utils;
+
 public class RemoveDuplicates {
+    public static int[] DuplicatesRemoved(int[] arr){
+        int i=0, j=1;
+        while(j < arr.length){
+            if(arr[i] == arr[j] || arr[j] < arr[i]) j++;
+            else {
+                i++;
+                Utils.swap(arr,i,j);
+                j++;
+            }
+        }
+        return arr;
+    }
 }
