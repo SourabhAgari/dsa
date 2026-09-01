adb43107974021fc3f8aa01764b14b548f93b9b6
sourabhragari <agarisourabh@gmail.com>
2026-09-01 20:36:23 +0530

    added problem statement as comment

--- src/main/java/com/healthcare/arrays/slidingwindow/longestSubarrayWithKDistinct.java
+++ src/main/java/com/healthcare/arrays/slidingwindow/longestSubarrayWithKDistinct.java
@@ -3,10 +3,32 @@ package com.healthcare.arrays.slidingwindow;
 import java.util.HashMap;
 import java.util.Map;
 
-
-// note : atmost and atleast slight variation
-// <= means atmost
-// >= means atleast
+/**
+ * Problem Statement:
+ *
+ * Given an integer array arr and an integer k, find the length of the
+ * longest contiguous subarray that contains exactly k distinct elements.
+ *
+ * Example:
+ * arr = [1, 2, 1, 2, 3]
+ * k = 2
+ *
+ * Subarrays with exactly 2 distinct elements:
+ * [1, 2]       -> length = 2
+ * [1, 2, 1]    -> length = 3
+ * [1, 2, 1, 2] -> length = 4
+ * [2, 1, 2]    -> length = 3
+ * [1, 2]       -> length = 2
+ *
+ * Output:
+ * 4
+ *
+ * Approach:
+ * Use a variable-size sliding window with a HashMap to keep track
+ * of the frequency of each element in the current window.
+ *
+ * Slight variation - atmost (<=) and atleast(>=) can be solved
+ */
 public class longestSubarrayWithKDistinct {
     public static int getLongestSubarrayWithKDistinct(int[] arr, int k){
         int left = 0, maxSum = Integer.MIN_VALUE;
