6e79c607fe44e1607158b78c49d9832525e92955
sourabhragari <agarisourabh@gmail.com>
2026-09-01 08:11:15 +0530

    tests for loop variation

--- src/test/java/com/healthcare/arrays/slidingwindow/MaximumSumSubArrayKTest.java
+++ src/test/java/com/healthcare/arrays/slidingwindow/MaximumSumSubArrayKTest.java
@@ -5,26 +5,26 @@ class MaximumSumSubArrayKTest {
     @Test
     void returnsMaximumSumForAWindowOfThree() {
         int[] numbers = {2, 1, 5, 1, 3, 2};
-        assertEquals(9, MaximumSumSubArrayK.getMaxSubArray(numbers, 3));
+        assertEquals(9, MaximumSumSubArrayK.getMaxSubArrayTwo(numbers, 3));
     }
     @Test
     void returnsLargestValueForAWindowOfOne() {
         int[] numbers = {2, -1, 5, 1};
-        assertEquals(5, MaximumSumSubArrayK.getMaxSubArray(numbers, 1));
+        assertEquals(5, MaximumSumSubArrayK.getMaxSubArrayTwo(numbers, 1));
     }
     @Test
     void handlesAnArrayContainingOnlyNegativeValues() {
         int[] numbers = {-2, -3, -1, -4};
-        assertEquals(-4, MaximumSumSubArrayK.getMaxSubArray(numbers, 2));
+        assertEquals(-4, MaximumSumSubArrayK.getMaxSubArrayTwo(numbers, 2));
     }
     @Test
     void returnsTotalWhenWindowMatchesArrayLength() {
         int[] numbers = {1, 2, 3};
-        assertEquals(6, MaximumSumSubArrayK.getMaxSubArray(numbers, 3));
+        assertEquals(6, MaximumSumSubArrayK.getMaxSubArrayTwo(numbers, 3));
     }
     @Test
     void findsMaximumAcrossOverlappingWindows() {
         int[] numbers = {1, 4, 2, 10, 23, 3, 1, 0, 20};
-        assertEquals(39, MaximumSumSubArrayK.getMaxSubArray(numbers, 4));
+        assertEquals(39, MaximumSumSubArrayK.getMaxSubArrayTwo(numbers, 4));
     }
 }
