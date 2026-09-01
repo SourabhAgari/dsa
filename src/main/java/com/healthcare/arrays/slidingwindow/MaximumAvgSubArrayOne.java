aa1451cd9466ec4746f30304a9a47b42750be97a
sourabhragari <agarisourabh@gmail.com>
2026-09-01 20:32:37 +0530

    added prob statement as comment

--- src/main/java/com/healthcare/arrays/slidingwindow/MaximumAvgSubArrayOne.java
+++ src/main/java/com/healthcare/arrays/slidingwindow/MaximumAvgSubArrayOne.java
@@ -1,5 +1,26 @@
 package com.healthcare.arrays.slidingwindow;
 
+/**
+ * Problem Statement:
+ *
+ * Given an integer array arr and an integer k, find the contiguous
+ * subarray of exactly k elements that has the maximum average value.
+ *
+ * Example:
+ * arr = [1, 12, -5, -6, 50, 3]
+ * k = 4
+ *
+ * Subarrays of size 4:
+ * [1, 12, -5, -6]  -> average = 0.5
+ * [12, -5, -6, 50] -> average = 12.75
+ * [-5, -6, 50, 3]  -> average = 10.5
+ *
+ * Output:
+ * 12.75
+ *
+ * Approach:
+ * Use a fixed-size sliding window of size k.
+ */
 public class MaximumAvgSubArrayOne {
     public static double getMaxAvgSubArray(int[] arr,int k){
         int left = 0, right = k, sum = 0;
