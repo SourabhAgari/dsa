b3b5374578bbbe1fe02ee9982e40615265b58887
sourabhragari <agarisourabh@gmail.com>
2026-09-01 08:10:57 +0530

    loop variation

--- src/main/java/com/healthcare/arrays/slidingwindow/MaximumSumSubArrayK.java
+++ src/main/java/com/healthcare/arrays/slidingwindow/MaximumSumSubArrayK.java
@@ -21,4 +21,26 @@ public class MaximumSumSubArrayK {
         }
         return maxSum;
     }
+
+    public static int getMaxSubArrayTwo(int[] arr, int k){
+        int maxSum = Integer.MIN_VALUE, sum = 0;
+        int n = arr.length;
+        int left = 0, right = k;
+        for(int i=0;i<k;i++){
+            sum += arr[i];
+        }
+
+        maxSum = Math.max(maxSum, sum);
+
+        while(right < n){
+            sum -= arr[left];
+            sum += arr[right];
+
+            maxSum = Math.max(maxSum,sum);
+
+            left++;
+            right++;
+        }
+        return maxSum;
+    }
 }
